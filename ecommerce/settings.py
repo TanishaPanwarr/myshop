5c9ac4c7a0ac0c08452d59862f9d70f1e154cd34
Manoj Mewada <mewadamanoj948@example.com>
2022-05-23 00:24:17 +0530

    make it better

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -166,5 +166,5 @@ RAZOR_KEY_SECRET = 'PDC9NQJh4X67bn7etnXaMlEN'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
